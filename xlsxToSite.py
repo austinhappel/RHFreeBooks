1662aab8eee7cd92ca7bd67adfad8593a3a46f14
Austin Happel <austin.happel@smashingideas.com>
2013-11-15 11:25:22 -0600

    updated for 11-13 book list, made book list more friendly

--- xlsxToSite.py
+++ xlsxToSite.py
@@ -13,44 +13,63 @@ import pystache
 CWD = os.getcwd()
 template = open(os.path.join(CWD, 'templates/index.html')).read()
 outputFile = open(os.path.join(CWD, 'index.html'), 'w')
-# wb = load_workbook('../Free book offering August 2013.xlsx')
-wb = load_workbook('test.xlsx')
-ws = wb.get_active_sheet()
-isbn_numbers = [isbn.value for isbn in ws.columns[0]][1:]
-titles = [title.value for title in ws.columns[1]][1:]
-format = [format.value for format in ws.columns[5]][1:]
-filter_format_options = []
-book_data = []
+wb = load_workbook('Employee Free Book list for 11-13.xlsx')
+# wb = load_workbook('test-11-13.xlsx')
 
-for idx, isbn in enumerate(isbn_numbers):
-    url = 'http://www.goodreads.com/book/isbn?format=XML&isbn=%s&key=fiEuEOjmjwnpLto0wT4KzA' % isbn
-    try:
-        response = urllib2.urlopen(url)
-    except urllib2.HTTPError:
-        print "SKIPPING %s" % isbn
-        continue
+worksheets = [
+    wb.get_active_sheet(),
+    wb.get_sheet_by_name('eBooks')
+]
 
-    data = response.read()
-    xml = etree.fromstring(data)
-    book_details = {}
-    book = xml.find('book')
-    book_details['title'] = titles[idx]
-    book_details['isbn'] = isbn
-    book_details['format'] = format[idx]
+def generateJSONFromXLSX(ws):
 
-    if format[idx] not in filter_format_options:
-        filter_format_options.append(format[idx])
+    isbn_numbers = [isbn.value for isbn in ws.columns[0]][1:]
+    titles = [title.value for title in ws.columns[1]][1:]
+    format = [format.value for format in ws.columns[4]][1:]
+    filter_format_options = []
+    book_data = []
 
-    book_details['cover_image'] = book.find('image_url').text
-    book_details['rating'] = book.find('average_rating').text
-    book_details['ratings_count'] = book.find('ratings_count').text
-    book_details['description'] = book.find('description').text
-    book_details['reviews_widget'] = book.find('reviews_widget').text
-    book_details['url'] = book.find('url').text
-    book_details['authors'] = [author.find('name').text for author in book.find('authors')]
-    book_data.append(book_details)
+    for idx, isbn in enumerate(isbn_numbers):
+        url = 'http://www.goodreads.com/book/isbn?format=XML&isbn=%s&key=fiEuEOjmjwnpLto0wT4KzA' % isbn
+        try:
+            response = urllib2.urlopen(url)
+        except urllib2.HTTPError:
+            print "SKIPPING %s" % isbn
+            continue
+
+        data = response.read()
+        xml = etree.fromstring(data)
+        book_details = {}
+        book = xml.find('book')
+        book_details['title'] = titles[idx]
+        book_details['isbn'] = isbn
+        book_details['format'] = format[idx]
+
+        if format[idx] not in filter_format_options:
+            filter_format_options.append(format[idx])
+
+        book_details['cover_image'] = book.find('image_url').text
+        book_details['rating'] = book.find('average_rating').text
+        book_details['ratings_count'] = book.find('ratings_count').text
+        book_details['description'] = book.find('description').text
+        book_details['reviews_widget'] = book.find('reviews_widget').text
+        book_details['url'] = book.find('url').text
+        book_details['authors'] = [author.find('name').text for author in book.find('authors')]
+        book_data.append(book_details)
+        
     # for verbosity:
     print json.dumps(book_data, indent=4)
+    return (book_data, filter_format_options)
+
+# initialize
+
+book_data = []
+filter_format_options = []
+ 
+for ws in worksheets:
+    data = generateJSONFromXLSX(ws)
+    book_data += data[0]
+    filter_format_options += data[1]
 
 context = {
     'books': book_data,
